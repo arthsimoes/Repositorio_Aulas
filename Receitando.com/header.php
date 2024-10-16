6365ee4c65be4114316c8eaa9137d2818ed4f9fc
Arthur Simões <1arthursimoes@gmail.com>
2024-10-16 10:59:51 -0300

    update

---
+++ Receitando.com/header.php
@@ -0,0 +1,9 @@
+<header>
+    <img src="ícones/icone_laranja-removebg-preview.png" alt="Ícone">
+    <h1>RECEITANDO.COM</h1>
+    <nav>
+        <a href="salgados.html"><img src="ícones/talheres-cruzados24.png" alt="Salgados" class="nav-icon"> SALGADOS</a>
+        <a href="doces.html"><img src="ícones/talheres-cruzados24.png" alt="Doces" class="nav-icon"> DOCES</a>
+        <a href="bebidas.html"><img src="ícones/talheres-cruzados24.png" alt="Bebidas" class="nav-icon"> BEBIDAS</a>
+    </nav>
+</header>
